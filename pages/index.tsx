95fe48596872491e717dcbd7768218242f3da53d
Alekseymedvedev <61234001+Alekseymedvedev@users.noreply.github.com>
2023-01-06 11:58:29 +0300

    Доработки

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ import React from "react";
 
 export default function Home() {
     return (
-        <MainLayout title="Главная страница">
+        <MainLayout title="Роблокс Адопт Ми трейды 2023 — сайт для проверки">
             <CompareSection/>
         </MainLayout>
     )
