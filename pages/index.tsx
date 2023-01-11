c862cf676b0edad2318aa54db31366177323a93a
Alekseymedvedev <61234001+Alekseymedvedev@users.noreply.github.com>
2023-01-11 09:30:18 +0300

    Правки

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,9 @@ import React from "react";
 
 export default function Home() {
     return (
-        <MainLayout title="Роблокс Адопт Ми трейды 2023 — сайт для проверки">
+        <MainLayout title="Роблокс Адопт Ми трейды 2023 — сайт для проверки"
+        content="Roblox Adopt Me trading - обменивайте своих питомцев, предметы и аксессуары Adopt Me, чтобы создать идеальную коллекцию виртуальных питомцев на популярной игровой платформе Roblox"
+        >
             <CompareSection/>
         </MainLayout>
     )
