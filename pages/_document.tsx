d44f46116fdae9b86d22f96013c81ae5e52aec77
Alekseymedvedev <61234001+Alekseymedvedev@users.noreply.github.com>
2023-01-12 15:55:03 +0300

    Метрика

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -13,23 +13,25 @@ export default function Document() {
                 dangerouslySetInnerHTML={{
                     __html: `
                     
-                    (function(m,e,t,r,i,k,a){m[i]=m[i]function(){(m[i].a=m[i].a[]).push(arguments)};
-                        m[i].l=1*new Date(); 
-                        for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-                        k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-                        (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-                        ym(91959069, "init", { clickmap:true, 
-                        trackLinks:true, 
-                        accurateTrackBounce:true, 
-                        webvisor:true 
-                    });
-                           
+                      (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                       m[i].l=1*new Date();
+                       for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+                       k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+                       (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+                    
+                       ym(92033844, "init", {
+                            clickmap:true,
+                            trackLinks:true,
+                            accurateTrackBounce:true,
+                            webvisor:true
+                       });
+
               `,
                 }}
             />
             <noscript>
                 <div>
-                    <img src="https://mc.yandex.ru/watch/91959069" style={{position: 'absolute', left: '-9999px'}}
+                    <img src="https://mc.yandex.ru/watch/92033844"  style={{position: 'absolute', left: '-9999px'}}
                          alt=""/>
                 </div>
             </noscript>
